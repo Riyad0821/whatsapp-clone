36560ff55e0547031365ba09fbdf20ea88772359
Riyad0821 <mdriyad0821@gmail.com>
2020-12-03 20:22:00 +0600

    Design almost done

---
+++ src/Chat.tsx
@@ -0,0 +1,63 @@
+import { Avatar, IconButton } from '@material-ui/core';
+import { AttachFile, MoreVert, SearchOutlined, SettingsInputAntenna } from '@material-ui/icons';
+import InsertEmoticonIcon from '@material-ui/icons/InsertEmoticon';
+import React from 'react';
+import './Chat.css';
+
+function Chat() {
+    return (
+        <div className="chat">
+            <div className="chat_header">
+                <Avatar />
+                <div className="chat_headerInfo">
+                    <h3>Room name</h3>
+                    <p>Last seen at...</p>
+                </div>
+                <div className="chat_headerRight">
+                    <IconButton>
+                        <SearchOutlined />
+                    </IconButton>
+                    <IconButton>
+                        <AttachFile />
+                    </IconButton>
+                    <IconButton>
+                        <MoreVert />
+                    </IconButton>
+                </div>
+            </div>
+            <div className="chat_body">
+                <p className="chat_message">
+                    <span className="chat_name">Sonny</span>
+                    This is a message
+                    <span className="chat_timestamp">{new Date().toUTCString()}</span>
+                </p>
+
+                <p className="chat_message">
+                    <span className="chat_name">Sonny</span>
+                    This is a message
+                    <span className="chat_timestamp">{new Date().toUTCString()}</span>
+                </p>
+
+                <p className="chat_message chat_receiver">
+                    <span className="chat_name">Sonny</span>
+                    This is a message
+                    <span className="chat_timestamp">{new Date().toUTCString()}</span>
+                </p>
+            </div>
+            <div className="chat_footer">
+                <InsertEmoticonIcon />
+                <form>
+                    <input
+                    // value=(input)
+                    // onCharge={(e) => SettingsInputAntenna(e.target.value)}
+                    placeholder="Type a message"
+                    type="text"
+                    />
+                    <button type="submit">Send a message</button>
+                </form>
+            </div>
+        </div>
+    )
+}
+
+export default Chat
